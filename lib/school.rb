4260ad1797e87a906b61e8a0b9cbb3a751264b0e
Victor Avila <avilavictor1570@gmail.com>
2020-07-23 04:04:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,17 @@
 class School
+  
   def initialize(name)
     @name = name
     @roster = {}
   end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, grade)
+    @student = name
+    @grade = grade
+  end
+  
 end
